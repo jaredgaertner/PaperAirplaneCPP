9ab2cc885b2f218fb748c56b82561592f9ed4e15
https://github.com/jaredgaertner <jared.gaertner@gmail.com>
2016-06-16 21:52:38 -0700

    Adding in content.

---
+++ Source/PaperAirplaneCPP/PaperAirplaneCPPGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PaperAirplaneCPPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPERAIRPLANECPP_API APaperAirplaneCPPGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
